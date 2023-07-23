43f8f142f50fc685c41e5a57c134736fc0a00166
yfnf7 <yfnf72@gmail.com>
2023-07-23 18:46:29 +0300

    created base config of the project

---
+++ src/main/java/com/example/foodcollector/FoodCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodcollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodCollectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodCollectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/foodcollector/FoodCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodcollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodCollectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
