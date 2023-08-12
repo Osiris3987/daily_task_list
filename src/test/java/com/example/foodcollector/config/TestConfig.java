d7cf997de3f130576c58dc192fb69e882bb9c7b6
yfnf7 <yfnf72@gmail.com>
2023-08-12 23:34:17 +0300

    Made tests for TaskServiceImpl

---
+++ src/test/java/com/example/foodcollector/config/TestConfig.java
@@ -0,0 +1,83 @@
+package com.example.foodcollector.config;
+
+import com.example.foodcollector.repository.TaskRepository;
+import com.example.foodcollector.repository.UserRepository;
+import com.example.foodcollector.service.Impl.AuthServiceImpl;
+import com.example.foodcollector.service.Impl.TaskServiceImpl;
+import com.example.foodcollector.service.Impl.UserServiceImpl;
+import com.example.foodcollector.service.props.JwtProperties;
+import com.example.foodcollector.web.security.JtwUsersDetailsService;
+import com.example.foodcollector.web.security.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@TestConfiguration
+@RequiredArgsConstructor
+public class TestConfig {
+
+    private final UserRepository userRepository;
+    private final TaskRepository taskRepository;
+    private final AuthenticationManager authenticationManager;
+
+    @Bean
+    @Primary
+    public BCryptPasswordEncoder testPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtProperties jwtProperties() {
+        JwtProperties jwtProperties = new JwtProperties();
+        jwtProperties.setSecret(
+                "dmdqYmhqbmttYmNhamNjZWhxa25hd2puY2xhZWtic3ZlaGtzYmJ1dg=="
+        );
+        return jwtProperties;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new JtwUsersDetailsService(userService());
+    }
+
+    @Bean
+    public Configuration configuration() {
+        return Mockito.mock(Configuration.class);
+    }
+
+    @Bean
+    public JwtTokenProvider tokenProvider() {
+        return new JwtTokenProvider(jwtProperties(),
+                userDetailsService(),
+                userService());
+    }
+
+    @Bean
+    @Primary
+    public UserServiceImpl userService() {
+        return new UserServiceImpl(
+                userRepository,
+                testPasswordEncoder()
+        );
+    }
+
+    @Bean
+    @Primary
+    public TaskServiceImpl taskService() {
+        return new TaskServiceImpl(taskRepository, userService());
+    }
+
+    @Bean
+    @Primary
+    public AuthServiceImpl authService() {
+        return new AuthServiceImpl(authenticationManager,
+                userService(),
+                tokenProvider());
+    }
+}
---
+++ src/test/java/com/example/foodcollector/service/Impl/TaskServiceImplTest.java
@@ -0,0 +1,104 @@
+package com.example.foodcollector.service.Impl;
+
+import com.example.foodcollector.config.TestConfig;
+import com.example.foodcollector.domain.exception.ResourceNotFoundException;
+import com.example.foodcollector.domain.task.Status;
+import com.example.foodcollector.domain.task.Task;
+import com.example.foodcollector.repository.TaskRepository;
+import com.example.foodcollector.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@Import(TestConfig.class)
+@ExtendWith(MockitoExtension.class)
+public class TaskServiceImplTest {
+
+    @MockBean
+    private TaskRepository taskRepository;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private AuthenticationManager authenticationManager;
+
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    private TaskServiceImpl taskService;
+
+    @Test
+    void getById() {
+        Long id = 1L;
+        Task task = new Task();
+        task.setId(id);
+        Mockito.when(taskRepository.findById(id))
+                .thenReturn(Optional.of(task));
+        Task testTask = taskService.getById(id);
+        Mockito.verify(taskRepository).findById(id);
+        Assertions.assertEquals(task, testTask);
+    }
+
+    @Test
+    void getByIdWithNoExistingId(){
+        Long id = 1L;
+        Mockito.when(taskRepository.findById(id)).thenReturn(Optional.empty());
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> taskService.getById(id));
+        Mockito.verify(taskRepository).findById(id);
+    }
+
+    @Test
+    void getAllByUserId(){
+        Long userId = 1L;
+        List<Task> tasks = new ArrayList<>();
+        for(int i = 0; i < 5; i++){
+            tasks.add(new Task());
+        }
+        Mockito.when(taskRepository.findAllByUserId(1L)).thenReturn(tasks);
+        List<Task> testList = taskService.getAllByUserId(userId);
+        Mockito.verify(taskRepository).findAllByUserId(userId);
+        Assertions.assertEquals(tasks.size(), testList.size());
+    }
+
+    @Test
+    void update(){
+        Task task = new Task();
+        task.setId(1L);
+        task.setTitle("watch smth");
+        task.setDescription("description");
+        task.setExpirationDate(LocalDateTime.now());
+        task.setStatus(Status.DONE);
+        Task testTask = taskService.update(task);
+        Mockito.verify(taskRepository).save(task);
+        Assertions.assertEquals(task, testTask);
+    }
+
+    @Test
+    void updateWithEmptyStatus(){
+        Task task = new Task();
+        task.setId(1L);
+        task.setTitle("watch smth");
+        task.setDescription("description");
+        task.setExpirationDate(LocalDateTime.now());
+        Task testTask = taskService.update(task);
+        Mockito.verify(taskRepository).save(task);
+        Assertions.assertEquals(testTask.getStatus(), Status.TODO);
+    }
+
+}
